20c4f3ed232f105c3ea8d3e29beab8116c266c3d
Michal Karwowski <mkarwo2003@gmail.com>
2025-11-02 14:07:44 +0100

    Commit1

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,24 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0)
+        return 0; // nie tworzymy ¿adnych statków
+
+    Stocznia     stocznia{};
+    unsigned int suma            = 0;
+    unsigned int liczbaZaglowcow = 0;
+
+    while (suma < towar) {
+        Statek*      s            = stocznia();       // tworzymy nowy statek
+        unsigned int przewiezione = s->transportuj(); // transportuje towar
+        suma += przewiezione;                         // sumujemy
+
+        // sprawdzamy, czy to ¿aglowiec
+        if (dynamic_cast< Zaglowiec* >(s) != nullptr)
+            ++liczbaZaglowcow;
+
+        delete s; // niszczymy statek
+    }
+
+    return liczbaZaglowcow;
 }
